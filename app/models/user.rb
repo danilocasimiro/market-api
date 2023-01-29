49f3061e6c48d3d4f49a8086b6185841a53d3536
Danilo Casimiro <dan_casimiro@hotmail.com>
2023-01-29 11:24:48 +0000

    Add attribute active in user model and add some validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :encrypted_password, length: { minimum: 6 }
+    validates :status, inclusion: [true, false]
+    validates :email, format: { with: /\a(.+)@(.+)\z/, message: 'Email invalid' },
+                      uniqueness: { case_sensitive: false },
+                      length: { minimum: 4, maximum: 254 }
+  end
 end
